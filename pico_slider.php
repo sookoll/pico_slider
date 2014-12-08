a35b325f6014917b2762979992a358bd79f252ce
Mihkel Oviir <mihkel.oviir@gmail.com>
2014-12-09 00:52:51 +0200

    Images sorting by name

--- pico_slider.php
+++ pico_slider.php
@@ -39,8 +39,9 @@ class Pico_Slider {
 	private function get_files($directory, $ext = '')
 	{
 		$array_items = array();
-		if($handle = opendir($directory)){
-			while(false !== ($file = readdir($handle))){
+		if($handle = scandir($directory)){
+			natsort($handle);
+            foreach ($handle as $file) {
 				if($file != "." && $file != ".."){
 					if(is_dir($directory. "/" . $file)){
 						$array_items = array_merge($array_items, $this->get_files($directory. "/" . $file, $ext));
@@ -50,7 +51,6 @@ class Pico_Slider {
 					}
 				}
 			}
-			closedir($handle);
 		}
 		return $array_items;
 	}
